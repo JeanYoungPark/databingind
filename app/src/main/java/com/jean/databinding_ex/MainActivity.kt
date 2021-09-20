b8a251ac5a765f3bcbdf7a8091eeb2ed704fb8e9
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-09-20 14:14:49 +0900

    Initial commit

---
+++ app/src/main/java/com/jean/databinding_ex/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.jean.databinding_ex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.jean.databinding_ex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
